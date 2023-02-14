34a0dcf1c3824e313c2e6709fca08f2256c7b8e2
Sebastian Ocampo <73962362+sebasop320@users.noreply.github.com>
2023-02-14 15:04:44 -0500

    Add files via upload

--- src/routes/colegio/+page.svelte
+++ src/routes/colegio/+page.svelte
@@ -1,7 +1,7 @@
 <title>School dasboard</title>
 
 <h1> Hello</h1>
-<h2>please fill out this fourm</h2>
+<h2>Please fill out this fourm</h2>
 <a href="https://nextcloud-pi.sebasopdev.com/index.php/apps/forms/s/gpPXeDdfk9NMCDwiXDxjnKtJ">Click here to fill out the fourm</a>
 <p>thanks</p>
 
@@ -9,23 +9,23 @@
 <h1> Site map</h1>
 
 <li>
-<a href="./">home</a>
+<a href="./">Home</a>
 </li>
 
 <li>
-<a href="./sverdle">game</a>
+<a href="./sverdle">Game</a>
 </li>
 
 <li>
-<a href="./about">about page</a>
+<a href="./about">About page</a>
 </li>
 
 <li>
-<a href="./contact">contact page</a>
+<a href="./contact">Contact page</a>
 </li>
 
 <li>
-    <a href="./favicon.png">favicon</a>
+    <a href="./favicon.png">Favicon</a>
     </li>
 
 
